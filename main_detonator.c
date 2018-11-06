fb20effd11cac996ea4fa18b914973834e28a1cc
luispons <luispons@gmail.com>
2018-11-06 14:55:33 +0100

    fts detonator working

--- main_detonator.c
+++ main_detonator.c
@@ -3,14 +3,11 @@
 //#include "persist.h"
 //#include "power.h"
 #include "board.h"
-#include "mavlink.h"
 #include <kernel/dispatcher.h>
 #include <kernel/panic.h>
 #include <support/stm32f1/wdt.h>
 #include <meta/config.h>
 #include <stdio.h>
-
-#include "datalog.h"
 
 //#define ENABLE_WATCHDOG
 
@@ -18,18 +15,16 @@ static dispatcher_t _led_off_dispatcher;
 static void _led_off(dispatcher_context_t *context, dispatcher_t *dispatcher);
 static void _led_flash(led_mask_t mask, unsigned time);
 
-static void _heartbeat(const mavlink_handler_t *handler, const mavlink_msg_t *msg, unsigned length);
-
 static dispatcher_context_t _context;
-
 
 static event_t _rx_event;
 static uart_control_block_t _cb;
 static dispatcher_t _rx_dispatcher;
-//static mutex_t _handler_mutex;
-static void _rx_dispatch(dispatcher_context_t *context, dispatcher_t *dispatcher);
+
+static dispatcher_t _firestarter_dispatcher;
 
 // Just bridge the messages to the log
+static void _rx_dispatch(dispatcher_context_t *context, dispatcher_t *dispatcher);
 
 int board_fts_read(unsigned char *buffer, unsigned long length);
 int board_fts_write(unsigned char *buffer, unsigned long length);
@@ -43,12 +38,20 @@ static void _rx_dispatch(dispatcher_context_t *context, dispatcher_t *dispatcher
 	unsigned char buffer[16];
 
 	int read = board_fts_read(buffer, sizeof(buffer));
-	//if (read > 0)
-	// printf ("%s", buffer);
+	if (read > 0)
+	 printf ("%s", buffer);
 
 	dispatcher_add(context, dispatcher, TIMEOUT_NEVER);
 }
 
+
+static void _firestarter_handler(dispatcher_context_t *context, dispatcher_t *dispatcher)
+{
+	if (board_detect_lines (0))
+		board_fts_write("DETONATE\n", 9);
+
+	dispatcher_add(&_context, &_firestarter_dispatcher, 50);
+}
 
 void main()
  {
@@ -56,17 +59,7 @@ void main()
 	board_set_led(-1, LED_GREEN); // enabled
 
 	printf("\n\nDetonator board boot!\n");
-
-	//printf ("detonator lines: %d %d\n",  board_detect_lines (0), board_detect_lines (1));
-
-   	// Fire test
-	/*thread_sleep(1000);
-	board_enable_charges(true);
-	board_fire(1);
-	thread_sleep(800);
-	board_fire(0);
-  	board_enable_charges(false);*/
-
+ 
 	dispatcher_context_create(&_context);
 	dispatcher_create(&_led_off_dispatcher, nullptr, _led_off, nullptr);
 
@@ -79,10 +72,11 @@ void main()
 
 	board_fts_init(&_context, &_cb);
 
-	//mutex_create(&_handler_mutex);
 	dispatcher_create(&_rx_dispatcher, &_rx_event, _rx_dispatch, nullptr);
 	dispatcher_add(&_context, &_rx_dispatcher, TIMEOUT_NEVER);
+	dispatcher_create(&_firestarter_dispatcher, nullptr, _firestarter_handler, nullptr);
 
+	dispatcher_add(&_context, &_firestarter_dispatcher, 50);
 
 #ifdef ENABLE_WATCHDOG
 	wdt_initialize(100);
@@ -93,22 +87,19 @@ void main()
 #ifdef ENABLE_WATCHDOG
 		wdt_reload();
 #endif
+
 		if (!dispatcher_dispatch(&_context, 300))
 		{
-			if (board_detect_lines (0))
-				board_fts_write("FIRE", 6);
-
-        	printf ("uga %d %d\n", board_detect_lines (0), board_detect_lines (1));
 			_led_flash(LED_RGB, 300);
 		}
 	}
 }
 
-static void _heartbeat(const mavlink_handler_t *handler, const mavlink_msg_t *msg, unsigned length)
+/*static void _heartbeat(const mavlink_handler_t *handler, const mavlink_msg_t *msg, unsigned length)
 {
-	//printf ("heart of hearts\n");
-	//dispatcher_add(&_context, &_deadman_dispatcher, 1800); // 1500	
-}
+	printf ("heart of hearts\n");
+	dispatcher_add(&_context, &_deadman_dispatcher, 1800); // 1500	
+}*/
 
 
 
