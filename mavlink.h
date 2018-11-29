2bf7a80a8f15d4003f0e7c1aebfacae911040fb9
luispons <luispons@gmail.com>
2018-11-29 13:33:11 +0100

    some mavlink flags

--- mavlink.h
+++ mavlink.h
@@ -75,9 +75,9 @@ typedef struct __packed
 
 typedef struct  {
 	unsigned int time_boot_ms; // [ms] Timestamp (time since system boot).
-	signed int lat; // [degE7] Latitude, expressed
-	signed int lon; // [degE7] Longitude, expressed
-	signed int alt; // [mm] Altitude (MSL). Note that virtually all GPS modules provide both WGS84 and MSL.
+	signed int lat;			// [degE7] Latitude, expressed
+	signed int lon;			// [degE7] Longitude, expressed
+	signed int alt;			// [mm] Altitude (MSL). Note that virtually all GPS modules provide both WGS84 and MSL.
 	signed int relative_alt; // [mm] Altitude above ground
 	signed short vx;       // [cm/s] Ground X Speed (Latitude, positive north)
 	signed short vy;       // [cm/s] Ground Y Speed (Longitude, positive east)
@@ -149,6 +149,18 @@ typedef enum
     MAV_DATA_STREAM_EXTRA2,
     MAV_DATA_STREAM_EXTRA3,
 } mavlink_data_stream_t;
+
+
+typedef enum{
+	MAV_MODE_FLAG_SAFETY_ARMED = 128,	// 0b10000000 MAV safety set to armed. Motors are enabled / running / can start. Ready to fly.
+	MAV_MODE_FLAG_MANUAL_INPUT_ENABLED = 64,	//0b01000000 remote control input is enabled.
+	MAV_MODE_FLAG_HIL_ENABLED = 32,		// 0b00100000 hardware in the loop simulation. All motors / actuators are blocked, but internal software is full operational.
+	MAV_MODE_FLAG_STABILIZE_ENABLED = 16, // 0b00010000 system stabilizes electronically its attitude (and optionally position). It needs however further control inputs to move around.
+	MAV_MODE_FLAG_GUIDED_ENABLED = 8, // 0b00001000 guided mode enabled, system flies waypoints / mission items.
+	MAV_MODE_FLAG_AUTO_ENABLED = 4, // 0b00000100 autonomous mode enabled, system finds its own goal positions. Guided flag can be set or not, depends on the actual implementation.
+	MAV_MODE_FLAG_TEST_ENABLED = 2, //0b00000010 system has a test mode enabled. This flag is intended for temporary system tests and should not be used for stable implementations.
+	MAV_MODE_FLAG_CUSTOM_MODE_ENABLED = 1  // 0b00000001 Reserved for future use.
+} mavlink_mode_flag_t;
 
 typedef struct __mavlink_handler mavlink_handler_t;
 typedef void (*mavlink_handler_func_t)(const mavlink_handler_t *handler, const mavlink_msg_t *msg, unsigned length);
